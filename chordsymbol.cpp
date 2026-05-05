4d159bc7366615a9b41624f582904a8f25a16c78
Judd Niemann <juddniemann@gmail.com>
2026-05-05 23:56:16 +1000

    updated chord symbol enum

--- chordsymbol.cpp
+++ chordsymbol.cpp
@@ -1,2 +1,70 @@
 #include "chordsymbol.h"
 
+#include <vector>
+
+std::string ChordSymbol::toHtml()
+{
+	std::vector<std::string> parts;
+
+	// root note
+	parts.push_back(root);
+
+	// base chord type
+	if (quality & ChordQualityFlags::Sus2)
+		parts.emplace_back(u8"sus2");
+
+	if (quality & ChordQualityFlags::Dim)
+		parts.emplace_back(u8"\u00B0");
+
+	if (quality & ChordQualityFlags::HalfDim)
+		parts.emplace_back(u8"\u00F8");
+
+	if (quality & ChordQualityFlags::Min)
+		parts.emplace_back(u8"m");
+
+	// if (quality & ChordQualityFlags::Maj)
+	// 	parts.emplace_back(u8"\u0394");
+
+	if (quality & ChordQualityFlags::Aug)
+		parts.emplace_back(u8"+");
+
+	if (quality & ChordQualityFlags::Sus)
+		parts.emplace_back(u8"sus");
+
+	if (quality & ChordQualityFlags::Sus4)
+		parts.emplace_back(u8"sus4");
+
+	// number
+	if (quality & ChordQualityFlags::Six)
+		parts.emplace_back(u8"6");
+
+	if (quality & ChordQualityFlags::Seven)
+		parts.emplace_back(u8"7");
+
+	if (quality & ChordQualityFlags::Maj7)
+		parts.emplace_back(u8"\u0394" "7");
+
+	if (quality & ChordQualityFlags::Nine)
+		parts.emplace_back(u8"9");
+
+	if (quality & ChordQualityFlags::Maj9)
+		parts.emplace_back(u8"\u0394" "9");
+
+	if (quality & ChordQualityFlags::Eleven)
+		parts.emplace_back(u8"11");
+
+	if (quality & ChordQualityFlags::Maj11)
+		parts.emplace_back(u8"\u0394" "11");
+
+	if (quality & ChordQualityFlags::Thirteen)
+		parts.emplace_back(u8"13");
+
+	if (quality & ChordQualityFlags::Maj13)
+		parts.emplace_back(u8"\u0394" "13");
+
+	// todo: all the extensions
+
+	// todo: put the parts together
+	return {};
+
+}
--- chordsymbol.h
+++ chordsymbol.h
@@ -6,71 +6,81 @@
 #include <memory>
 #include <string>
 
-// These Flags need to be sufficient to capture print any chord without
+// These Flags need to be sufficient to print any chord without
 // actually using any music theory; just instructions for what to print
 
-enum class ChordQualityFlags : int64_t
+enum class ChordQualityFlags : uint64_t
 {
-	NC       = 0x0, // No chord
+	Empty    = 0x0, // Don't display a chord symbol
 
-	// base chord type
-	Omit3    = 0x1,
-	Sus2     = 0x2,
-	Sus2Post = 0x4, // C7sus2 etc
-	Dim      = 0x8,
-	HalfDim  = 0x10,
-	Min      = 0x20,
-	Maj      = 0x40,
-	Aug      = 0x80,
-	Sus      = 0x100,
-	SusPost  = 0x200,
-	Sus4     = 0x400,
-	Sus4Post = 0x800, // C7sus4, C13sus4 etc
+	// base chord/triad type
+	Sus2     = 0x1,
+	Dim      = 0x2,
+	HalfDim  = 0x4,
+	Min      = 0x8,
+	Maj      = 0x10,
+	Aug      = 0x20,
+	Sus      = 0x40,
+	Sus4     = 0x80,
 
-	Sharp4   = 0x1000,
+	// extensions
+	Six      = 0x100,   // add a "6" : "C6, Cm6"
+	Seven    = 0x200,   // Just add "7" to whatever is already there
+	Maj7     = 0x400,   // for Maj7th chords only
+	Nine     = 0x800,
+	Maj9     = 0x1000,
+	SixNine  = 0x2000,
+	Eleven   = 0x4000,
+	Maj11    = 0x8000,
+	Thirteen = 0x10000,
+	Maj13    = 0x20000,
 
-	// types of 5th (always post-)
-	Omit5    = 0x2000,
-	Flat5    = 0x4000,
-	Sharp5   = 0x8000,
+	// alterations
+	Omit3    = 0x40000,
+	Add2     = 0x80000,
+	Sus2Post = 0x100000,  // C7sus2 etc
+	SusPost  = 0x200000,
+	Sus4Post = 0x400000,  // C7sus4, C13sus4 etc
+	Sharp4   = 0x800000,
+	Omit5    = 0x1000000,
+	Flat5    = 0x2000000,
+	Sharp5   = 0x4000000,
+	Min6Post  = 0x8000000,   // add "min6" eg Cm (min6) - aeolian sound
+	Flat6Post = 0x10000000,  // add flat-6 eg C (b6) ;  or CDim(b6) etc
+	Aug6      = 0x20000000,  // may be required for French/Italian/German 6ths ?
+	Maj7Post  = 0x40000000,  // Cm(maj7), Cdim(maj7)
+	Flat9     = 0x80000000,
+	Add9      = 0x100000000,
+	Sharp9    = 0x200000000,
+	Add11     = 0x400000000,
+	Sharp11   = 0x800000000,
+	Flat13    = 0x1000000000,
+	Alt       = 0x2000000000, // altered chord
+	Sharp15   = 0x4000000000,
 
-	// types of 6th
-	Min6Post = 0x10000, // add "min6" eg Cm (min6) - aeolian sound
-	Flat6Post = 0x20000, // add flat-6 eg C (b6) ;  or CDim(b6) etc
-	Six      = 0x40000, // add a "6" : "C6, Cm6"
-	Aug6     = 0x80000, // may be required for French/Italian/German 6ths ?
-
-	// types of 7ths
-	Seven    = 0x100000, // Just add "7" to whatever is already there
-	Maj7     = 0x200000, // for Major chords
-	Maj7Post = 0x400000, // Cm(maj7), Cdim(maj7)
-
-	// types of 9ths
-	Flat9    = 0x800000,
-	Nine     = 0x1000000,
-	SixNine  = 0x2000000,
-	Sharp9   = 0x4000000,
-
-	// types of 11ths
-	Eleven   = 0x8000000,
-	Sharp11  = 0x10000000,
-
-	// types of 13ths
-	Flat13   = 0x20000000,
-	Thirteen = 0x40000000,
-
-	// sharp15
-	Sharp15  = 0x80000000
+	// NC (sentinel)
+	NC = 0x8000000000000000 // No Chord (Symbol to explicitly say "N.C.")
 };
 
+inline constexpr ChordQualityFlags operator|(ChordQualityFlags lhs, ChordQualityFlags rhs) {
+	return static_cast<ChordQualityFlags>(
+		static_cast<uint64_t>(lhs) | static_cast<uint64_t>(rhs)
+	);
+}
+
+inline constexpr uint64_t operator&(ChordQualityFlags lhs, ChordQualityFlags rhs) {
+	return static_cast<uint64_t>(lhs) & static_cast<uint64_t>(rhs);
+}
 
 struct ChordSymbol
 {
 	std::string root;
-	int64_t quality{0ll};
+	ChordQualityFlags quality{0ll};
 	std::string bass;
 
 	std::unique_ptr<ChordSymbol> denominator; // optional for building polychords (possibly recursively ...)
+
+	std::string toHtml();
 };
 
 #endif // CHORDSYMBOL_H
